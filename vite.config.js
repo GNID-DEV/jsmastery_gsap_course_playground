e5344b3db678dfddb5947229408ac4764f4de521
Vince Ding <vince.ding@gmail.com>
2025-08-31 20:29:39 +0800

    gh-page commit

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: '/jsmastery_gsap_course_playground/',
   build: {
     rollupOptions: {
       input: {
